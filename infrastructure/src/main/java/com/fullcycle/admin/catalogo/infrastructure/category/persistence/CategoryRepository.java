c29bb87fb0bfcd3ea3b0f1ffed1ef23b9b41ae83
Wellington Cruz <wellington.max1@gmail.com>
2024-05-04 11:29:54 -0300

    Implementação inicial para repository e acesso ao Mysql de category.

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/persistence/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.fullcycle.admin.catalogo.infrastructure.category.persistence;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryJpaEntity, String> {
+}
