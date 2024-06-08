3e8268ecb346c280e0e80388fe995070c88af886
Wellington Cruz <wellington.max1@gmail.com>
2024-06-08 17:08:12 -0300

    Correção de imports.

--- application/src/main/java/com/fullcycle/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
@@ -3,13 +3,12 @@ package com.fullcycle.admin.catalogo.application.category.create;
 import com.fullcycle.admin.catalogo.domain.category.Category;
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
 import com.fullcycle.admin.catalogo.domain.validation.handler.Notification;
-import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
-import io.vavr.API;
 import io.vavr.control.Either;
 
 import java.util.Objects;
 
-import static io.vavr.API.*;
+import static io.vavr.API.Left;
+import static io.vavr.API.Try;
 
 public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
 
--- application/src/test/java/com/fullcycle/admin/catalogo/application/UseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/UseCaseTest.java
@@ -1,9 +1,5 @@
 package com.fullcycle.admin.catalogo.application;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 //@ExtendWith(MockitoExtension.class)
 public class UseCaseTest {
 
