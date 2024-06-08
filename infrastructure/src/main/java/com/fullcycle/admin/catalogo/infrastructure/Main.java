837f8227d162a99e19f5ba33d8f3264077742a2e
Wellington Cruz <wellington.max1@gmail.com>
2024-06-07 21:08:23 -0300

    Removendo hello world

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -7,8 +7,7 @@ import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(final String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
