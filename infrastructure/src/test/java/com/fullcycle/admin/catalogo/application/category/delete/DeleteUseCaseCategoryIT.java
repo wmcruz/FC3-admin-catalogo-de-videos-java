b2467eff3068387c3dcd19526009f2367abdcad6
Wellington Cruz <wellington.max1@gmail.com>
2024-06-10 21:56:26 -0300

    Adicionando testes de integração de delete e get de categoria.

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/category/delete/DeleteUseCaseCategoryIT.java
@@ -0,0 +1,74 @@
+package com.fullcycle.admin.catalogo.application.category.delete;
+
+import com.fullcycle.admin.catalogo.IntegrationTest;
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@IntegrationTest
+public class DeleteUseCaseCategoryIT {
+
+    @Autowired
+    private DeleteCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCategory_shouldBeOK() {
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
+        final var expectedId = aCategory.getId();
+
+        save(aCategory);
+
+        assertEquals(1, categoryRepository.count());
+
+        assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        assertEquals(0, categoryRepository.count());
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsDeleteCategory_shouldBeOK() {
+        final var expectedId = CategoryID.from("123");
+
+        assertEquals(0, categoryRepository.count());
+
+        assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        assertEquals(0, categoryRepository.count());
+    }
+
+    @Test
+    public void givenAValidId_whenGatewayThrowsException_shouldReturnException() {
+        final var expectedId = CategoryID.from("123");
+
+        doThrow(new IllegalStateException("Gateway error"))
+                .when(categoryGateway).deleteById(eq(expectedId));
+
+        assertThrows(IllegalStateException.class, () -> useCase.execute(expectedId.getValue()));
+        verify(categoryGateway, times(1)).deleteById(expectedId);
+    }
+
+    private void save(final Category... aCategory) {
+        categoryRepository.saveAllAndFlush(
+                Arrays.stream(aCategory)
+                        .map(CategoryJpaEntity::from)
+                        .toList()
+        );
+    }
+}
---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCaseIT.java
@@ -0,0 +1,89 @@
+package com.fullcycle.admin.catalogo.application.category.retrieve.get;
+
+import com.fullcycle.admin.catalogo.IntegrationTest;
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@IntegrationTest
+public class GetCategoryByIdUseCaseIT {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private GetCategoryByIdUseCase useCase;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidId_whenCallsGetCategory_shouldReturnCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var expectedId = aCategory.getId();
+
+        save(aCategory);
+
+        final var actualCategory = useCase.execute(expectedId.getValue());
+
+        assertEquals(expectedId, actualCategory.id());
+        assertEquals(expectedName, actualCategory.name());
+        assertEquals(expectedDescription, actualCategory.description());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertEquals(aCategory.getCreatedAt().truncatedTo(ChronoUnit.MINUTES), actualCategory.createdAt().truncatedTo(ChronoUnit.MINUTES));
+        assertEquals(aCategory.getUpdatedAt().truncatedTo(ChronoUnit.MINUTES), actualCategory.updatedAt().truncatedTo(ChronoUnit.MINUTES));
+        assertEquals(aCategory.getDeletedAt(), actualCategory.deletedAt());
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsGetCategory_shouldReturnNotFound() {
+        final var expectedErrorMessage = "Category with ID 123 was not found";
+        final var expectedId = CategoryID.from("123");
+
+        final var actualException = assertThrows(DomainException.class, () -> useCase.execute(expectedId.getValue()));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
+    @Test
+    public void givenAValidId_whenGatewayThrowsException_shouldReturnException() {
+        final var expectedErrorMessage = "Gateway error";
+        final var expectedId = CategoryID.from("123");
+
+        doThrow(new IllegalStateException(expectedErrorMessage))
+                .when(categoryGateway).findById(Mockito.eq(expectedId));
+
+        final var actualException = assertThrows(IllegalStateException.class, () -> useCase.execute(expectedId.getValue()));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
+    private void save(final Category... aCategory) {
+        categoryRepository.saveAllAndFlush(
+                Arrays.stream(aCategory)
+                        .map(CategoryJpaEntity::from)
+                        .toList()
+        );
+    }
+}
