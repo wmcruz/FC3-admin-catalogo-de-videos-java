1180117b5623c1278468b7f819a1979365ef16ed
Wellington Cruz <wellington.max1@gmail.com>
2024-07-08 19:02:36 -0300

    Implementação de testes de marshall e unmarshall para objetos json e pojos.

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/JacksonTest.java
@@ -0,0 +1,23 @@
+package com.fullcycle.admin.catalogo;
+
+import com.fullcycle.admin.catalogo.infrastructure.configuration.ObjectMapperConfig;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test")
+@JsonTest(includeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ObjectMapperConfig.class)
+})
+public @interface JacksonTest {
+}
