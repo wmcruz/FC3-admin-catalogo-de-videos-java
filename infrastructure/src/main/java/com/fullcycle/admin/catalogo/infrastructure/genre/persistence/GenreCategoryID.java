7d9a5290f422dec0e1c5b1825768b7c6973789c9
Wellington Cruz <wellington.max1@gmail.com>
2024-11-06 15:12:55 -0300

    Criação de relacionamento entre JPA e BD.

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/persistence/GenreCategoryID.java
@@ -0,0 +1,55 @@
+package com.fullcycle.admin.catalogo.infrastructure.genre.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class GenreCategoryID {
+
+    @Column(name = "genre_id", nullable = false)
+    private String genreId;
+
+    @Column(name = "category_id", nullable = false)
+    private String categoryId;
+
+    private GenreCategoryID() {}
+
+    public GenreCategoryID(final String aGenreId, final String aCategoryId) {
+        this.genreId = aGenreId;
+        this.categoryId = aCategoryId;
+    }
+
+    public static GenreCategoryID from(final String aGenreId, final String aCategoryId) {
+        return new GenreCategoryID(aGenreId, aCategoryId);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final GenreCategoryID that = (GenreCategoryID) o;
+        return Objects.equals(genreId, that.genreId) && Objects.equals(categoryId, that.categoryId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(genreId, categoryId);
+    }
+
+    public String getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(String genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+}
