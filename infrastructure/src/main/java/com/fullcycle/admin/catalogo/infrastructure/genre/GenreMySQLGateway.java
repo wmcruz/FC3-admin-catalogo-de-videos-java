335b1a36a3d7f2df124986abbf0fa6863307c18c
Wellington Cruz <wellington.max1@gmail.com>
2024-12-03 22:28:52 -0300

    Implementação de findAll para GenreMySQLGateway

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/GenreMySQLGateway.java
@@ -7,10 +7,16 @@ import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
 import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
 import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreJpaEntity;
 import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import com.fullcycle.admin.catalogo.infrastructure.utils.SpecificationsUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.Specification.where;
 
 @Component
 public class GenreMySQLGateway implements GenreGateway {
@@ -46,8 +52,30 @@ public class GenreMySQLGateway implements GenreGateway {
     }
 
     @Override
-    public Pagination<Genre> findAll(SearchQuery aQuery) {
-        return null;
+    public Pagination<Genre> findAll(final SearchQuery aQuery) {
+        final var page = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Sort.Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        final var where = Optional.ofNullable(aQuery.terms())
+                .filter(str -> !str.isBlank())
+                .map(this::assembleSpecification)
+                .orElse(null);
+
+        final var pageResults = genreRepository.findAll(where(where), page);
+
+        return new Pagination<>(
+                pageResults.getNumber(),
+                pageResults.getSize(),
+                pageResults.getTotalElements(),
+                pageResults.map(GenreJpaEntity::toAggregate).toList()
+        );
+    }
+
+    private Specification<GenreJpaEntity> assembleSpecification(final String terms) {
+        return SpecificationsUtils.like("name", terms);
     }
 
     private Genre save(final Genre aGenre) {
