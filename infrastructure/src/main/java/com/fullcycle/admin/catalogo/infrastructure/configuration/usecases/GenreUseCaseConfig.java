0a56e02a76801849d0ec053896322345d3bf17ac
Wellington Cruz <wellington.max1@gmail.com>
2024-12-05 19:27:39 -0300

    Criação de configuração para injeção de gateway nos casos de uso.

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/usecases/GenreUseCaseConfig.java
@@ -0,0 +1,54 @@
+package com.fullcycle.admin.catalogo.infrastructure.configuration.usecases;
+
+import com.fullcycle.admin.catalogo.application.genre.create.CreateGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.create.DefaultCreateGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.delete.DefaultDeleteGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.delete.DeleteGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.get.DefaultGetGenreByIdUsecase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.get.GetGenreByIdUseCase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.list.DefaultListGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.list.ListGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.update.DefaultUpdateGenreUseCase;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.genre.GenreGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class GenreUseCaseConfig {
+
+    private final CategoryGateway categoryGateway;
+    private final GenreGateway genreGateway;
+
+    public GenreUseCaseConfig(final CategoryGateway categoryGateway, final GenreGateway genreGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+        this.genreGateway = Objects.requireNonNull(genreGateway);
+    }
+
+    @Bean
+    public CreateGenreUseCase createGenreUseCase() {
+        return new DefaultCreateGenreUseCase(categoryGateway, genreGateway);
+    }
+
+    @Bean
+    public DeleteGenreUseCase deleteGenreUseCase() {
+        return new DefaultDeleteGenreUseCase(genreGateway);
+    }
+
+    @Bean
+    public GetGenreByIdUseCase getGenreByIdUseCase() {
+        return new DefaultGetGenreByIdUsecase(genreGateway);
+    }
+
+    @Bean
+    public ListGenreUseCase listGenreUseCase() {
+        return new DefaultListGenreUseCase(genreGateway);
+    }
+
+    @Bean
+    public DefaultUpdateGenreUseCase defaultUpdateGenreUseCase() {
+        return new DefaultUpdateGenreUseCase(categoryGateway, genreGateway);
+    }
+}
