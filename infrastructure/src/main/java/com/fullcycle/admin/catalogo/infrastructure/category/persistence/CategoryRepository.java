d6a14694e65bf5fe82e3d4f01dffe5683bac3bc4
Wellington Cruz <wellington.max1@gmail.com>
2024-12-17 18:57:52 -0300

    Implementação da consulta de vários ids de categoria.

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/persistence/CategoryRepository.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/persistence/CategoryRepository.java
@@ -4,8 +4,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<CategoryJpaEntity, String> {
 
     Page<CategoryJpaEntity> findAll(Specification<CategoryJpaEntity> whereClause, Pageable page);
+
+    @Query(value = "SELECT c.id FROM Category c WHERE c.id IN :ids")
+    List<String> existsByIds(@Param("ids") List<String> ids);
 }
