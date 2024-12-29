32c687171d9b1e2ccb4ca5bccaec2c7f9cc5a601
Wellington Cruz <wellington.max1@gmail.com>
2024-12-29 18:32:05 -0300

    Criação de update testes para domínio de CastMember.

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CastMemberTest {
 
@@ -102,6 +103,129 @@ public class CastMemberTest {
         final var actualException = assertThrows(
                 NotificationException.class,
                 () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        // then
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAValidCastMember_whenCallUpdate_shouldReceiveUpdated() {
+        // given
+        final var expectedName = "Vin Diesel";
+        final var expectedType = CastMemberType.ACTOR;
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+        assertNotNull(actualMember);
+
+        final var actualId = actualMember.getId();
+        final var actualCreatedAt = actualMember.getCreatedAt();
+        final var actualUpdatedAt = actualMember.getUpdatedAt();
+
+        // when
+        actualMember.update(expectedName, expectedType);
+
+        // then
+        assertEquals(actualId, actualMember.getId());
+        assertEquals(expectedName, actualMember.getName());
+        assertEquals(expectedType, actualMember.getType());
+        assertEquals(actualCreatedAt, actualMember.getCreatedAt());
+        assertTrue(actualUpdatedAt.isBefore(actualMember.getUpdatedAt()));
+    }
+
+    @Test
+    public void givenAValidCastMember_whenCallUpdateWithInvalidNullName_shouldReceiveNotification() {
+        // given
+        final String expectedName = null;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+        assertNotNull(actualMember);
+        assertNotNull(actualMember.getId());
+
+        // when
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
+        );
+
+        // then
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAValidCastMember_whenCallUpdateWithInvalidEmptyName_shouldReceiveNotification() {
+        // given
+        final var expectedName = " ";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+        assertNotNull(actualMember);
+        assertNotNull(actualMember.getId());
+
+        // when
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
+        );
+
+        // then
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAValidCastMember_whenCallUpdateWithInvalidNullType_shouldReceiveNotification() {
+        // given
+        final var expectedName = "Vin Diesel";
+        final CastMemberType expectedType = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+        assertNotNull(actualMember);
+        assertNotNull(actualMember.getId());
+
+        // when
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
+        );
+
+        // then
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAValidCastMember_whenCallUpdateWithLenghtMoreThan255_shouldReceiveANotification() {
+        // given
+        final var expectedName = "   Do mesmo modo, a mobilidade dos capitais internacionais garante a contribuição de um grupo importante na determinação do remanejamento dos quadros funcionais. " +
+                "A certificação de metodologias que nos auxiliam a lidar com o desenvolvimento contínuo de distintas formas de atuação prepara-nos para enfrentar situações atípicas decorrentes dos procedimentos normalmente adotados. " +
+                "As experiências acumuladas demonstram que a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do sistema de formação de quadros que corresponde às necessidades.";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+
+        assertNotNull(actualMember);
+        assertNotNull(actualMember.getId());
+
+        // when
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
         );
 
         // then
