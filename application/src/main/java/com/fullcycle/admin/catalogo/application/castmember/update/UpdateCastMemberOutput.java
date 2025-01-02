a3127ae86d4da8f22bdb983e48d21ad44c68d87c
Wellington Cruz <wellington.max1@gmail.com>
2025-01-02 09:46:45 -0300

    Criação das implementações e testes de unidade para o usecase de Update de CastMember.

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/castmember/update/UpdateCastMemberOutput.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalogo.application.castmember.update;
+
+import com.fullcycle.admin.catalogo.domain.castmember.CastMember;
+
+public record UpdateCastMemberOutput(String id) {
+    public static UpdateCastMemberOutput from(final CastMember aMember) {
+        return new UpdateCastMemberOutput(aMember.getId().getValue());
+    }
+}
