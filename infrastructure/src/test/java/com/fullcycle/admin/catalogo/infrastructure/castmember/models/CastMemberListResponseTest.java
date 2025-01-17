7d1858ed7fea35a49651a41fa4dcc44cce197836
Wellington Cruz <wellington.max1@gmail.com>
2025-01-17 07:41:01 -0300

    Criação de teste de marshal para CastMemberListResponse.

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/castmember/models/CastMemberListResponseTest.java
@@ -0,0 +1,40 @@
+package com.fullcycle.admin.catalogo.infrastructure.castmember.models;
+
+import com.fullcycle.admin.catalogo.Fixture;
+import com.fullcycle.admin.catalogo.JacksonTest;
+import com.fullcycle.admin.catalogo.domain.utils.InstantUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JacksonTest
+public class CastMemberListResponseTest {
+
+    @Autowired
+    private JacksonTester<CastMemberListResponse> json;
+
+    @Test
+    public void marshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type().name();
+        final var expectedCreatedAt = InstantUtils.now().toString();
+
+        final var response = new CastMemberListResponse(
+                expectedId,
+                expectedName,
+                expectedType,
+                expectedCreatedAt
+        );
+
+        final var actualJson = this.json.write(response);
+
+        assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.type", expectedType)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt);
+    }
+}
