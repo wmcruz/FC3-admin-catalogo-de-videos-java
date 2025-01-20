57cdd0cf8c2e612044c209361abd4468e26218f0
Wellington Cruz <wellington.max1@gmail.com>
2025-01-19 22:51:51 -0300

    Criação do agregado de Video.

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/VideoTest.java
@@ -0,0 +1,69 @@
+package com.fullcycle.admin.catalogo.domain.video;
+
+import com.fullcycle.admin.catalogo.domain.castmember.CastMemberID;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.domain.genre.GenreID;
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VideoTest {
+
+    @Test
+    public void givenValidParams_whenCallsNewVideo_shouldInstantiate() {
+        // given
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                Essa é uma descrição de teste para esse vídeo.""";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        // when
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        // then
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration());
+        assertEquals(expectedOpened, actualVideo.getOpened());
+        assertEquals(expectedPublished, actualVideo.getPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+}
